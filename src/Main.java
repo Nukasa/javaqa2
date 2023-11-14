6a9f1b39446113dd0ed187421858a293b123c12e
Nukasa <147660632+Nukasa@users.noreply.github.com>
2023-11-14 20:59:26 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int prise = 10000;
-        int miles = 20;
-        int bonus = prise / miles;
-        System.out.println("итоговый бонус:" + bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
-
